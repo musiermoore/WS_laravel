9a3feb9c1e30b137afdb67f48587a9e417ee0009
musiermoore <musiermoore@gmail.com>
2020-11-25 13:50:06 +0600

    Fixed typo in model name and migration - (PassAnger -> PassEnger)

--- database/migrations/2020_11_25_065307_create_passengers_table.php
+++ database/migrations/2020_11_25_065307_create_passengers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePassangersTable extends Migration
+class CreatePassengersTable extends Migration
 {
     /**
      * Run the migrations.
