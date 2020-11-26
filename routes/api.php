ebaffa18df2694c34f72f0f8cdc9bf05df63c379
musiermoore <musiermoore@gmail.com>
2020-11-26 11:49:56 +0600

    Moved routes from web.php to api.php

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([], function (){
+    Route::get('airport', 'AirportController@search')->name('airport');
+    Route::get('flight', 'FlightController@search')->name('flight');
+});
+
--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,8 @@
 |
 */
 
-use App\Http\Resources\AirportResource;
-use App\Models\Airport;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::prefix('api')->group(function () {
-//    Route::get('airport', function () {
-//        return AirportResource::collection(Airport::all());
-//        return Airport::find(1);
-//    });
-    Route::get('airport', 'AirportController@search')->name('airport');
 });
