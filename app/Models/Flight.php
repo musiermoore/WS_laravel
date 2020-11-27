941f197c763fee6238c2beb404575e420840a434
musiermoore <musiermoore@gmail.com>
2020-11-27 13:01:22 +0600

    Created Accessors to parse 'Time'

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
@@ -10,14 +11,45 @@ class Flight extends Model
     {
         return $this->hasOne(Airport::class, 'id', 'from_id');
     }
-
+    
     public function airportTo()
     {
         return $this->hasOne(Airport::class, 'id', 'to_id');
     }
 
+    /**
+     * @param $date
+     */
     public function setDate($date)
     {
         $this->date = $date;
+    }
+
+    /**
+     * Accessor for Time (from)
+     *
+     * @param $time
+     *
+     * @return string
+     */
+    public function getTimeFromAttribute($time)
+    {
+        $newTime = Carbon::parse($time)->format('H:i');
+
+        return $newTime;
+    }
+
+    /**
+     * Accessor for Time (to)
+     *
+     * @param $time
+     *
+     * @return string
+     */
+    public function getTimeToAttribute($time)
+    {
+        $newTime = Carbon::parse($time)->format('H:i');
+
+        return $newTime;
     }
 }
