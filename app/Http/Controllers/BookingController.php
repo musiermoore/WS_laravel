cbf7c558652a7d1bb74180711e743042d0644004
musiermoore <musiermoore@gmail.com>
2020-11-29 12:56:17 +0600

    Changes in field names in BookingController

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -13,10 +13,10 @@ class BookingController extends Controller
     {
         $rules = [
             'flight_from' => 'required',
-            'flight_from.id' => 'required|exists:flights,id',
-            'flight_from.date' => 'required|date_format:Y-m-d',
-            'flight_back.id' => 'required_with:flight_back|exists:flights,id',
-            'flight_back.date' => 'required_with:flight_back|date_format:Y-m-d',
+            'flight_from_id' => 'required|exists:flights,id',
+            'flight_from_date' => 'required|date_format:Y-m-d',
+            'flight_back_id' => 'required_with:flight_back|exists:flights,id',
+            'flight_back_date' => 'required_with:flight_back|date_format:Y-m-d',
         ];
 
         $data = $request->input();
@@ -34,17 +34,18 @@ class BookingController extends Controller
         }
 
         $bookingData = [
-            'flight_from' => $request->flight_from['id'],
-            'date_from' => $request->flight_from['date'],
+            'flight_from' => $request->flight_from_id,
+            'date_from' => $request->flight_from_date,
             'code' => Str::upper(Str::random(5)),
         ];
 
         if ($request->has('flight_back')) {
-            $bookingData['flight_back'] = $request->flight_back['id'];
-            $bookingData['date_back'] = $request->flight_back['date'];
+            $bookingData['flight_back'] = $request->flight_back_id;
+            $bookingData['date_back'] = $request->flight_back_date;
         }
-
+    
         $booking = Booking::create($bookingData);
+
 
         $result = [
             'data' => [
