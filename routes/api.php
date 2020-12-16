3dfedd4c9c740545f24dc14386d51ae082cdc50a
musiermoore <musiermoore@gmail.com>
2020-12-16 17:41:19 +0600

    Updated routes and auth config

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\AirportController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\FlightController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
